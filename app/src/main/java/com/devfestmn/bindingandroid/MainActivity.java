b4f7940d440a76d89352a9d6d55a5e01a3ae2abd
rpgobjects <rpgobjects@gmail.com>
2016-04-08 17:36:55 -0500

    android studio 2.0 updates

--- app/src/main/java/com/devfestmn/bindingandroid/MainActivity.java
+++ app/src/main/java/com/devfestmn/bindingandroid/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.devfestmn.bindingandroid.databinding.ObservablecollectionBinding;
-
 public class MainActivity extends AppCompatActivity {
 
 
@@ -18,52 +16,64 @@ public class MainActivity extends AppCompatActivity {
 
         // NO Binding
         Button button = (Button) findViewById(R.id.pojo_binding_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, PojoActivity.class));
-            }
-        });
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, PojoActivity.class));
+                }
+            });
+        }
 
         button = (Button) findViewById(R.id.baseobservable_binding_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,BaseObservableActivity.class));
-            }
-        });
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this,BaseObservableActivity.class));
+                }
+            });
+        }
 
         button = (Button) findViewById(R.id.observablefield_binding_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,ObservableFieldActivity.class));
-            }
-        });
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this,ObservableFieldActivity.class));
+                }
+            });
+        }
 
         button = (Button) findViewById(R.id.observablecollection_binding_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,ObservableCollectionActivity.class));
-            }
-        });
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this,ObservableCollectionActivity.class));
+                }
+            });
+        }
 
         button = (Button) findViewById(R.id.recyclerview_binding_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,RecyclerViewActivity.class));
-            }
-        });
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this,RecyclerViewActivity.class));
+                }
+            });
+        }
 
         button = (Button) findViewById(R.id.butterknife_binding_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,ButterKnifeActivity.class));
-            }
-        });
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this,ButterKnifeActivity.class));
+                }
+            });
+        }
     }
 
 }
